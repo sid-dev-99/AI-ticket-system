9e19691d1e80f393e1eb28134a9fab240216ddf3
siddharth <engrsiddharth1@gmail.com>
2024-09-08 22:39:58 +0530

    added chatbot ui

---
+++ src/app/bookingDetails/page.tsx
@@ -0,0 +1,7 @@
+export default function bookingDetails() {
+  return (
+    <div className="flex min-h-[100dvh] flex-col items-center justify-center  bg-cover bg-center bg-no-repeat px-4 py-12 sm:px-6 lg:px-8">
+     booking details
+    </div>
+  );
+}
