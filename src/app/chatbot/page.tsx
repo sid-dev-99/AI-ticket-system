317017dbdbeb4900f59e72ac408d63418751a7f7
siddharth <engrsiddharth1@gmail.com>
2024-09-08 21:04:37 +0530

    added landing page

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,7 @@
+export default function Chatbot() {
+    return (
+      <div className="flex min-h-[100dvh] flex-col items-center justify-center  bg-cover bg-center bg-no-repeat px-4 py-12 sm:px-6 lg:px-8">
+       hello from chatbot
+      </div>
+    );
+  }
